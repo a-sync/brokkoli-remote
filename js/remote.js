e7a2ed8fd486d69242ad6436c7706827b3c6c1b8
Smith <suckonmyrocket+git@gmail.com>
2017-01-25 00:25:26 +0100

    bump supported versions

--- js/remote.js
+++ js/remote.js
@@ -924,7 +924,7 @@ $("#default").removeClass("hidden");
 /*! Set variables. */
 var version = "2.1.1";
 var versionTag = ".alpha";
-var supportedPopcorntimeVersions = ["1.0.11","1.0.10","1.0.9","1.0.8","1.0.7","1.0.6","1.0.5","1.0.4"];
+var supportedPopcorntimeVersions = ["1.0.13","1.0.12","1.0.11","1.0.10","1.0.9","1.0.8","1.0.7","1.0.6","1.0.5","1.0.4"];
 var ip;
 var port;
 var username;
